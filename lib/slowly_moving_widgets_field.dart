60c9a156e18e87278fa12283d169a5a1c824a18f
Peter Alvin <peter.alvin@gmail.com>
2020-01-06 05:08:20 -0500

    creation collision detection solved

--- lib/slowly_moving_widgets_field.dart
+++ lib/slowly_moving_widgets_field.dart
@@ -5,6 +5,8 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 
+final r = new Random();
+
 class SlowlyMovingWidgetsField extends StatefulWidget {
   @override
   _SlowlyMovingWidgetsFieldState createState() =>
@@ -19,28 +21,37 @@ class Dude {
   double xdelta;
   double ydelta;
   Color color;
+  String id;
 
   double get right => left + width;
+
   double get bottom => top + height;
 
+//  void position(double s_width, double s_height) {
+//    left = r.nextInt((s_width - width).toInt()).toDouble();
+//    top = r.nextInt((s_height - height).toInt()).toDouble();
+//  }
+
   bool hit(Dude d) {
-    return d.left > left && d.left < right ||
-        d.right > left && d.right < right && d.top > top && d.top < bottom ||
-        d.bottom > top && d.bottom < bottom;
+    return (d.left > left && d.left < right ||
+            d.right > left && d.right < right) &&
+        (d.top > top && d.top < bottom || d.bottom > top && d.bottom < bottom);
   }
 
   bool hitx(Dude d) {
-    return hit(d) && (d.xdelta > 0 && xdelta < 0 || xdelta > 0 && d.xdelta < 0);
+//    return hit(d) && (d.xdelta > 0 && xdelta < 0 || xdelta > 0 && d.xdelta < 0);
+    return false;
   }
 
   bool hity(Dude d) {
-    return hit(d) && (d.ydelta > 0 && ydelta < 0 || ydelta > 0 && d.ydelta < 0);
+//    return hit(d) && (d.ydelta > 0 && ydelta < 0 || ydelta > 0 && d.ydelta < 0);
+    return false;
   }
+
+  String toString() => "dude $id: l=$left r=$right t=$top b=$bottom";
 }
 
 class _SlowlyMovingWidgetsFieldState extends State<SlowlyMovingWidgetsField> {
-  final r = new Random();
-
   double width = -1;
   double height = -1;
 
@@ -50,21 +61,67 @@ class _SlowlyMovingWidgetsFieldState extends State<SlowlyMovingWidgetsField> {
   int hitCnt = 0;
   int hitxCnt = 0;
   int hityCnt = 0;
-  int i = 0;
+  int colorRotate = 0;
+  int beats = 0;
   List<Dude> list = [];
-  Container back = Container(color: Colors.red);
+  Container back = Container(color: Colors.white);
 
   @override
   Widget build(BuildContext context) {
     if (height < 0) {
       width = MediaQuery.of(context).size.width;
       height = MediaQuery.of(context).size.height;
-      list.forEach((d) {
-        if (d.left < 0) {
+
+      for (int i = 0; i < 30; i++) {
+        int attempt = 0;
+        while (true) {
+          attempt++;
+
+          Dude d = Dude();
+          switch (colorRotate) {
+            case 0:
+              d.color = Color.fromARGB(
+                  255, 0, 200 + r.nextInt(56), 200 + r.nextInt(56));
+              break;
+            case 1:
+              d.color = Color.fromARGB(
+                  255, 200 + r.nextInt(56), 0, 200 + r.nextInt(56));
+              break;
+            case 2:
+              d.color = Color.fromARGB(
+                  255, 200 + r.nextInt(56), 200 + r.nextInt(56), 0);
+              break;
+          }
+
+          colorRotate++;
+          colorRotate %= 3;
+          d.xdelta = (0.5 - (r.nextDouble() * 3)) / 1;
+          d.ydelta = (0.5 - (r.nextDouble() * 0.5)) / 1;
+
+//          d.position(width, height);
           d.left = r.nextInt((width - d.width).toInt()).toDouble();
           d.top = r.nextInt((height - d.height).toInt()).toDouble();
+
+          bool add = true;
+          if (list.length > 0) {
+            list.forEach((d2) {
+//              print("cr: $d2");
+              if (d.hit(d2)) {
+//                print("$i attempt $attempt: $d");
+//                print("$i         $attempt: $d2");
+                add = false;
+              }
+            });
+          }
+
+          if (add) {
+            d.id = "$i";
+            print("created: $d");
+            list.add(d);
+            break;
+          }
         }
-      });
+      }
     }
 
 //    print("build");
@@ -108,9 +165,7 @@ class _SlowlyMovingWidgetsFieldState extends State<SlowlyMovingWidgetsField> {
 
             d.xdelta *= -1;
             d2.xdelta *= -1;
-          }
-
-          if (d.hity(d2)) {
+          } else if (d.hity(d2)) {
             hityCnt++;
 //            print("hity $hityCnt");
 
@@ -138,36 +193,12 @@ class _SlowlyMovingWidgetsFieldState extends State<SlowlyMovingWidgetsField> {
   void initState() {
     super.initState();
 
-    for (int i = 0; i < 10; i++) {
-      int attempt = 0;
-      while (true) {
-        attempt++;
-        Dude d = Dude();
-        d.color = Color.fromARGB(
-            255, 200 + r.nextInt(56), 200 + r.nextInt(56), 200 + r.nextInt(56));
-        d.xdelta = (0.5 - (r.nextDouble() * 2 + 0.1)) / 1;
-        d.ydelta = (0.5 - (r.nextDouble() + 0.1)) / 1;
-
-        bool add = true;
-        if (list.length > 0) {
-          list.forEach((d2) {
-            if (d.hit(d2)) {
-              print("attempt $attempt: creation hit");
-              add = false;
-            }
-          });
-        }
-
-        if (add) {
-          list.add(d);
-          break;
-        }
-      }
-    }
-
     Timer.periodic(Duration(milliseconds: 1000 ~/ 30), (timer) {
       setState(() {
-        i++;
+        beats++;
+        if (beats % 10000 == 0) {
+          print("10K beats");
+        }
       });
     });
   }
